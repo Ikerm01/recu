353f2085ff274939f92064f44189542edd09db8d
alumne <ikermacias10@gmail.com>
2024-12-10 14:18:39 +0100

    update

--- calcular_si_es_primo.py
+++ calcular_si_es_primo.py
@@ -1,5 +1,12 @@
+n = int(input("Ingrese un número: "))
+
 def es_primo(n):
     for i in range(2,n):
         if (n%i) == 0:
             return False
-    return True
+    return True 
+
+if es_primo(n):
+    print(f"{n} es un número primo")
+else:
+    print(f"{n} no es un número primo")
