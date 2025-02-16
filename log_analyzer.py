b32554df15e0120c8456b0c887a471fb658b0ecc
Iker Macias Jurado <151910000+Ikerm01@users.noreply.github.com>
2025-02-16 23:16:22 +0100

    funcion para generar el informe con los resultados del anilisis(obtener la ip unica no me ha salido por eso no aparece)

--- log_analyzer.py
+++ log_analyzer.py
@@ -22,3 +22,24 @@ def comptar_per_tipus(registre):
 
 def comptar_paraula_clau(registre, paraula_clau):
   return sum(linia.lower().count(paraula_clau.lower()) for linia in registre)
+
+def generar_informe(register.log, paraula_clau):
+  registre = llegir_fitxer(register.log)
+  if not registre:
+      return ""
+    
+  total_registres = comptar_registres(registre)
+  tipus_registres = comptar_per_tipus(registre)
+  paraula_comptatge = comptar_paraula_clau(registre, paraula_clau)
+    
+  informe = (f"Informe d'Anàlisi de Fitxer de Registre\n"
+             f"---------------------------------------\n"
+             f"Nombre total de registres: {total_registres}\n"
+             f"Registres per tipus:\n"
+             f"\tINFO: {tipus_registres['INFO']}\n"
+             f"\tWARNING: {tipus_registres['WARNING']}\n"
+             f"\tERROR: {tipus_registres['ERROR']}\n"
+             f"Recurrence de la paraula clau '{paraula_clau}': {paraula_comptatge}\n"
+             f"---------------------------------------")
+    
+  return informe
