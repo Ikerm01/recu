19f1eed7ad9e3dafef1f628cdd4d51e82da92874
Iker Macias Jurado <151910000+Ikerm01@users.noreply.github.com>
2025-02-16 22:22:07 +0100

    funcion para leer el archivo de regristos y que me devuelva las lineas

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,1 +1,10 @@
-
+def llegir_fitxer(register.log):
+  try:
+        with open(register.log, 'r', encoding='utf-8') as fitxer:
+            return fitxer.readlines()
+    except FileNotFoundError:
+        print(f"Error: No s'ha trobat el fitxer {register.log}.")
+        return []
+    except Exception as e:
+        print(f"Error inesperat: {e}")
+        return []
